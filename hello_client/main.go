4f52cd248856a4898bf13e74b2830bd791f4ffee
zhuzi <sanyewudezhuzi@163.com>
2023-03-27 13:59:00 +0800

    v2

--- hello_client/main.go
+++ hello_client/main.go
@@ -3,38 +3,46 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/sanyewudezhuzi/gRPC_study/pb"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 // hello_client
 
 const (
 	defaultName = "world"
+	cretFile    = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.pem" // 证书文件
+	defaultDNS  = "*.sanyewu.com"                                                           // openssl.cfg 文件中 [ alt_names ] 标签定义的 DSN
 )
 
 var (
-	addr = flag.String("addr", "127.0.0.1:9090", "IP address and port number of the tcp connection")
-	name = flag.String("name", defaultName, "Name to greet")
+	addr               = flag.String("addr", "127.0.0.1:9090", "IP address and port number of the tcp connection")
+	name               = flag.String("name", defaultName, "Name to greet")
+	serverNameOverride = flag.String("dns", defaultDNS, "Server name override")
 )
 
 func main() {
 	// 将用户传递至命令行的参数解析为对应变量值
 	flag.Parse()
-	// 连接到server端，此处禁用安全传输
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// 获取证书
+	creds, err := credentials.NewClientTLSFromFile(cretFile, *serverNameOverride)
+	if err != nil {
+		panic("failed to creds")
+	}
+	// 连接到server端
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
 	defer conn.Close()
 	// 建立连接
 	client := pb.NewGreeterClient(conn)
-
 	// 执行RPC调用并打印收到的响应数据（这个方法在服务端实现并返回结果）
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -42,5 +50,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to sayhello: %v", err)
 	}
-	log.Println(res.GetReply())
+	fmt.Println(res.GetReply())
 }
--- hello_server/main.go
+++ hello_server/main.go
@@ -8,9 +8,16 @@ import (
 	"github.com/sanyewudezhuzi/gRPC_study/pb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // hello_server
+
+const (
+	// 服务器的绝对路径
+	cretFile = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.pem" // 证书文件
+	keyFile  = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.key" // 私钥文件
+)
 
 type server struct {
 	pb.UnimplementedGreeterServer
@@ -22,13 +29,19 @@ func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloR
 }
 
 func main() {
+	// 生成证书
+	creds, err := credentials.NewServerTLSFromFile(cretFile, keyFile)
+	if err != nil {
+		panic("failed to creds")
+	}
 	// 监听本地的 9090 端口
 	listen, err := net.Listen("tcp", "127.0.0.1:9090")
 	if err != nil {
 		panic("failed to listen")
 	}
 	// 创建gRPC服务器
-	grpcServer := grpc.NewServer()
+	// 配置证书
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 	// 在 grpc 客户端注册我们自己编写的服务
 	pb.RegisterGreeterServer(grpcServer, &server{})
 	// 启动服务
