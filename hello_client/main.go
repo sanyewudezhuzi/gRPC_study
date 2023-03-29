adc83d7757620ec4a7d2f6a634b92939048e43d7
zhuzi <sanyewudezhuzi@163.com>
2023-03-29 10:49:24 +0800

    v3

--- hello_client/main.go
+++ hello_client/main.go
@@ -4,51 +4,58 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
 	"github.com/sanyewudezhuzi/gRPC_study/pb"
-
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 // hello_client
 
 const (
 	defaultName = "world"
-	cretFile    = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.pem" // 证书文件
-	defaultDNS  = "*.sanyewu.com"                                                           // openssl.cfg 文件中 [ alt_names ] 标签定义的 DSN
 )
 
 var (
-	addr               = flag.String("addr", "127.0.0.1:9090", "IP address and port number of the tcp connection")
-	name               = flag.String("name", defaultName, "Name to greet")
-	serverNameOverride = flag.String("dns", defaultDNS, "Server name override")
+	addr = flag.String("addr", "127.0.0.1:9090", "IP address and port number of the tcp connection")
+	name = flag.String("name", defaultName, "Name to greet")
 )
+
+func runLotsOfReplies(c pb.GreeterClient) {
+	// server端流式RPC
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := c.LotsOfReplies(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("c.LotsOfReplies failed, err: %v", err)
+	}
+	for {
+		// 接收服务端返回的流式数据，当收到io.EOF或错误时退出
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("c.LotsOfReplies failed, err: %v", err)
+		}
+		fmt.Println(res.GetReply())
+	}
+}
 
 func main() {
 	// 将用户传递至命令行的参数解析为对应变量值
 	flag.Parse()
-	// 获取证书
-	creds, err := credentials.NewClientTLSFromFile(cretFile, *serverNameOverride)
-	if err != nil {
-		panic("failed to creds")
-	}
 	// 连接到server端
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
 	defer conn.Close()
 	// 建立连接
 	client := pb.NewGreeterClient(conn)
-	// 执行RPC调用并打印收到的响应数据（这个方法在服务端实现并返回结果）
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	res, err := client.SayHello(ctx, &pb.HelloRequest{Name: *name})
-	if err != nil {
-		log.Fatalf("failed to sayhello: %v", err)
-	}
-	fmt.Println(res.GetReply())
+	// 客户端调用 LotsOfReplies 并将收到的数据依次打印出来
+	runLotsOfReplies(client)
 }
--- hello_server/main.go
+++ hello_server/main.go
@@ -1,47 +1,48 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net"
 
 	"github.com/sanyewudezhuzi/gRPC_study/pb"
-
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 // hello_server
-
-const (
-	// 服务器的绝对路径
-	cretFile = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.pem" // 证书文件
-	keyFile  = "E:\\GoLanguage\\src\\gRPC" + "\\gRPC_study\\hello_server\\key\\test.key" // 私钥文件
-)
 
 type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	fmt.Println("hello: " + req.Name)
-	return &pb.HelloResponse{Reply: "Hello " + req.Name}, nil
+func (s *server) LotsOfReplies(req *pb.HelloRequest, stream pb.Greeter_LotsOfRepliesServer) error {
+	fmt.Println(req.GetName())
+	words := []string{
+		"你好 ",
+		"hello ",
+		"こんにちは ",
+		"안녕하세요 ",
+		"สวัสดี ",
+	}
+	for _, word := range words {
+		data := &pb.HelloResponse{
+			Reply: word + req.GetName(),
+		}
+		// 使用 send 方法返回多个数据
+		if err := stream.Send(data); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
-	// 生成证书
-	creds, err := credentials.NewServerTLSFromFile(cretFile, keyFile)
-	if err != nil {
-		panic("failed to creds")
-	}
 	// 监听本地的 9090 端口
 	listen, err := net.Listen("tcp", "127.0.0.1:9090")
 	if err != nil {
 		panic("failed to listen")
 	}
 	// 创建gRPC服务器
-	// 配置证书
-	grpcServer := grpc.NewServer(grpc.Creds(creds))
+	grpcServer := grpc.NewServer()
 	// 在 grpc 客户端注册我们自己编写的服务
 	pb.RegisterGreeterServer(grpcServer, &server{})
 	// 启动服务
